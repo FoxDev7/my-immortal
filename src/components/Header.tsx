283112b57a114bc1184ce3402ebd833fed48b703
Kitsune7X <fox.dev7@proton.me>
2026-04-28 12:06:29 -0700

    basic header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+	return (
+		<div className="fixed top-0 right-0 left-0 z-50 w-full">
+			<header className="relative z-50 w-full tracking-tighter transition-all duration-300 bg-transparent">
+				<div className="relative mx-auto max-w-7xl px-6">
+					<div className="flex h-16 items-center justify-between gap-8">
+						<h1>Hello!</h1>
+					</div>
+				</div>
+			</header>
+		</div>
+	);
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,6 +6,7 @@ import {
 	Scripts,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import Header from "#/components/Header";
 import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
 import appCss from "../styles.css?url";
 
@@ -44,7 +45,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 				<HeadContent />
 			</head>
 			<body>
-				<h1>Hello</h1>
+				<Header />
 				{children}
 				<TanStackDevtools
 					config={{
