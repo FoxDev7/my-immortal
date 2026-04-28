ca25ac0195d2c7baf10af6c3ad6969169f1e8b6b
Kitsune7X <fox.dev7@proton.me>
2026-04-28 11:49:11 -0700

    header

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -24,7 +24,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 				content: "width=device-width, initial-scale=1",
 			},
 			{
-				title: "TanStack Start Starter",
+				title: "My Immortal",
 			},
 		],
 		links: [
@@ -44,6 +44,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 				<HeadContent />
 			</head>
 			<body>
+				<h1>Hello</h1>
 				{children}
 				<TanStackDevtools
 					config={{
