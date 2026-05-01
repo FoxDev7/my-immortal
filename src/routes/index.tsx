665654fc231a8dac813760c827e13655de407f37
Kitsune7X <fox.dev7@proton.me>
2026-05-01 12:05:16 -0700

    typo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ function Home() {
 	return (
 		<div className="grow w-full h-full">
 			<main className="w-full h-[calc(100svh-128px)]">
-				<img src="/hero.jpg" alt="Hero Image" className="h-full object-cover"/>
+				<img src="/hero.jpg" alt="Hero" className="h-full object-cover" />
 			</main>
 		</div>
 	);
